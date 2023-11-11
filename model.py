f8d11bc0dd0e761e78045388c8f27084d2378dc8
Matthew Savin <matt.asavin@gmail.com>
2023-11-11 19:15:16 +0000

    customer data field name change

--- model.py
+++ model.py
@@ -27,7 +27,7 @@ costOfBatteries = numberOfBatteries * 200
 #    return cost
 
 # Adds column to DataFrame with cost for each 15 minute interval
-df['cost_for_15m'] = df.apply(lambda x: (((x['load_power_kW']/x['NumberOfPanels'])*numberOfPanels - x['pv_totalPower_kW']) * x['price_total_NZDperkWh'])/4, axis=1)
+df['cost_for_15m'] = df.apply(lambda x: (((x['load_power_kW']/x['NumberOfPanels'])*numberOfPanels - x['pv_totalPower_kW']) * x['price_gridImport_NZDperkWh'])/4, axis=1)
 
 # Produces csv with final DataFrame
 df.to_csv('customerData_modified.csv', index=False, encoding='utf-8')
