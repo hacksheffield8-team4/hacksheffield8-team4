f26c2961f58f11508df88728f257e81e15db466c
Matthew Savin <matt.asavin@gmail.com>
2023-11-11 19:03:20 +0000

    calculate total cost for a customer

--- model.py
+++ model.py
@@ -5,12 +5,16 @@ df = pd.read_csv("customerData.csv")
 
 # Remove duplicate rows and save to new csv
 df_duplicates_removed = df.drop_duplicates()
-df.to_csv('customerData_new.csv', index=False, encoding='utf-8')
+# df.to_csv('customerData_new.csv', index=False, encoding='utf-8')
 
 # Sort csv by date and time
 df['Formatted_DateTime'] = pd.to_datetime(df['Date_UTC'])
 df = df.sort_values(by='Formatted_DateTime')
-df.to_csv('customerData_sortedByDateTime.csv', index=False)
+# df.to_csv('customerData_sortedByDateTime.csv', index=False)
+
+# Choose a specific customer by ID
+customerID = 15
+df = df.loc[df['customerID'] == customerID]
 
 batteryEfficiency = 0.92
 
@@ -20,12 +24,14 @@ numberOfBatteries = 1
 costOfPanels = numberOfPanels * 20
 costOfBatteries = numberOfBatteries * 200
 
-def calculateCost(loadPower, pvPower, priceOfEnergy, numberOfBatteries):
-    cost = ((loadPower - pvPower) * priceOfEnergy)/4
-    return cost
+#def calculateCost(loadPower, pvPower, priceOfEnergy, numberOfBatteries):
+#    cost = ((loadPower - pvPower) * priceOfEnergy)/4
+#    return cost
 
 df['cost_for_15m'] = df.apply(lambda x: ((x['load_power_kW'] - x['pv_totalPower_kW']) * x['price_total_NZDperkWh'])/4, axis=1)
-df.to_csv('customerData_with_costs.csv', index=False, encoding='utf-8')
+# df.to_csv('customerData_with_costs.csv', index=False, encoding='utf-8')
+
+print(df['cost_for_15m'].sum())
 
 # df['F(x)'] = df.loc[df['customerID'] == 62].mul(df['load_power_kW'], df['price_total_NZDperkWh'])
 # print(df['F(x)'])
