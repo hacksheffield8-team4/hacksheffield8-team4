f01e7a63319525867d246b3f6868efa50fc3149d
Matthew Savin <matt.asavin@gmail.com>
2023-11-11 19:13:04 +0000

    scaling power based on number of panels user has

--- model.py
+++ model.py
@@ -27,13 +27,13 @@ costOfBatteries = numberOfBatteries * 200
 #    return cost
 
 # Adds column to DataFrame with cost for each 15 minute interval
-df['cost_for_15m'] = df.apply(lambda x: ((x['load_power_kW'] - x['pv_totalPower_kW']) * x['price_total_NZDperkWh'])/4, axis=1)
+df['cost_for_15m'] = df.apply(lambda x: (((x['load_power_kW']/x['NumberOfPanels'])*numberOfPanels - x['pv_totalPower_kW']) * x['price_total_NZDperkWh'])/4, axis=1)
 
 # Produces csv with final DataFrame
 df.to_csv('customerData_modified.csv', index=False, encoding='utf-8')
 
 # Prints total cost for one year
-print(df['cost_for_15m'].sum())
+print(costOfPanels + df['cost_for_15m'].sum())
 
 # df['F(x)'] = df.loc[df['customerID'] == 62].mul(df['load_power_kW'], df['price_total_NZDperkWh'])
 # print(df['F(x)'])
