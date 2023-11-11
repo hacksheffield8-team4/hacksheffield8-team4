0ec5a6db12e85e742cf1cd0814faeff79b69ac31
Matthew Savin <matt.asavin@gmail.com>
2023-11-11 15:32:34 +0000

    add some code

--- model.py
+++ model.py
@@ -1,9 +1,23 @@
 import pandas as pd
 import numpy as np
-import time
 
-s_time = time.time()
 df = pd.read_csv("customerData.csv")
-e_time = time.time()
 
-print("Read without chunks: ", (e_time-s_time), "seconds") 
+batteryEfficiency = 0.92
+
+numberOfPanels = 18
+numberOfBatteries = 1
+
+def calculateCost(numberOfPanels, numberOfBatteries):
+    batteryCost = 3000
+    panelCost = 200
+    cost = numberOfBatteries * batteryCost + numberOfPanels * panelCost
+    
+    return cost
+
+df['F(x)'] = df.loc[df['customerID'] == 62].mul(df['load_power_kW'], df['price_total_NZDperkWh'])
+print(df['F(x)'])
+print(df.loc[df['customerID'] == 62])
+print(calculateCost(numberOfPanels, numberOfBatteries))
+
+print(df.dtypes)
