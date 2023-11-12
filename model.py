bd5fefdf4bc296e745d2455d937ccf652409352d
Matthew Savin <matt.asavin@gmail.com>
2023-11-12 00:18:38 +0000

    add column L - power supplied to load

--- model.py
+++ model.py
@@ -34,6 +34,8 @@ df['priceBeforeSolar'] = df['price_gridImport_NZDperkWh'] * df['load_power_kW']
 # Column K - Renewable energy to load before solar
 df['energy2loadPreSolar'] = df['grid_renewableFraction_pct']*df['load_power_kW']/4
 
+# Column L - Power supplied to load
+df['pvSuppliedToLoad'] = df[['powerAfterScaling', 'load_power_kW']]
 
 # Adds column to DataFrame with cost for each 15 minute interval
 df['cost_for_15m'] = df['price_gridImport_NZDperkWh'] * ((df['load_power_kW'] - (df['pv_totalPower_kW'] * newNumberOfPanels / df['NumberOfPanels'])) / 4)
